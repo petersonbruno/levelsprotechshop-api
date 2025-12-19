b397b47c3546114c6a9446e43f2ab7a2edc032c7
petersonbruno <aloyceptrsn@gmail.com>
2025-12-20 00:04:55 +0300

    add admin

--- config/settings.py
+++ config/settings.py
@@ -151,6 +151,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:8000",
     "http://127.0.0.1:3000",
     "http://127.0.0.1:8000",
+    "https://levelsprotechshop.vercel.app",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
