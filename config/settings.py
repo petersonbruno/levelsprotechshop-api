fa4340628b58f6184994587578f0f2ee39c9f41f
Peter Aloyce <aloyceptrsn@gmail.com>
2026-02-19 14:51:44 +0300

    security

--- config/settings.py
+++ config/settings.py
@@ -87,8 +87,11 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
-    {
+      {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'OPTIONS': {
+            'min_length': 4,
+        }
     },
     {
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
